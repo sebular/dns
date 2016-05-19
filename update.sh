e2f0c70f9a90e8913352f60e8d77f07840a5be56
Sebastian <sebastian.k.wood@gmail.com>
2016-05-18 23:50:10 -0700

    update IP on Wed May 18 23:50:10 PDT 2016

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 cd /home/sebastian/dns
+OLD_IP=$(cat ip.txt)
 IP=$(curl icanhazip.com)
-DATE=$(date)
-echo $IP >> ./ip-list.txt
-
-git commit -am "whatever $DATE" && git push
-
+if [ "$OLD_IP" != "$IP" ]; then
+  echo $IP > ./ip.txt
+  DATE=$(date)
+  git commit -am "update IP on $DATE" && git push
+fi
